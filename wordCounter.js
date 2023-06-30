58a5d228491dcb23f541ffd38294c5038a6862f2
Shabbir <shabbirdummy@gmail.com>
2023-06-30 09:03:04 +0530

    word-counter

---
+++ wordCounter.js
@@ -0,0 +1,20 @@
+let textbox = document.getElementById("textbox");
+
+textbox.addEventListener("input" , function(){
+  var text = this.value;
+  let char = text.length;
+  document.getElementById("char").innerHTML = char
+
+
+  text = text.trim()
+
+  let words = text.split(" ");
+
+  let cleanlist = words.filter(function(elm){
+    return elm != ""
+  })
+
+  console.log(words)
+  document.getElementById("word").innerHTML = cleanlist.length
+
+})
